198218535c3c474ce552037dc0badd1401a4f1d2
IgorJanicevic <milkomilkic@yahoo.com>
2023-12-12 17:36:55 +0100

    Device S
    S odradjen

--- ProjekatProxy/ProjekatProxy/Device/Device.cs
+++ ProjekatProxy/ProjekatProxy/Device/Device.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,52 +8,15 @@ using System.Threading.Tasks;
 namespace ProjekatProxy
 {
 
-    public class Measurement
-    {
-        public int DeviceID { get; private set; }
-        public bool IsAnalog { get; private set; }
-        public double Value { get; private set; }
-        public DateTime Timestamp { get; private set; }
-
-        public Measurement(int deviceID, bool isAnalog, double value)
-        {
-            DeviceID = deviceID;
-            IsAnalog = isAnalog;
-            Value = value;
-            Timestamp = DateTime.Now;
-        }
-    }
-
-
-
-
-
-    internal class Device
+    class Device
     {
         public int UniqueID { get; private set; }
         public List<Measurement> Measurements { get; private set; }
-
         public Device(int uniqueID)
         {
             UniqueID = uniqueID;
             Measurements = new List<Measurement>();
         }
-
-        public void RecordMeasurement(double value, bool isAnalog)
-        {        
-            Measurement measurement = new Measurement(UniqueID, isAnalog, value);
-            Measurements.Add(measurement);
-
-            // Pozovi metodu za slanje merenja
-            SendMeasurementToServer(measurement);
-        }
-
-        private void SendMeasurementToServer(Measurement measurement)
-        {
-            // Implementiraj logiku za slanje merenja na server
-            Console.WriteLine($"Device {UniqueID}: Measurement sent to server. Value: {measurement.Value}, Timestamp: {measurement.Timestamp}");
-        }
-
         public string GetMeasurementType()
         {
             // Povrati tip merenja poslednjeg merenja
@@ -62,6 +26,18 @@ namespace ProjekatProxy
 
             }
             return "Unknown";
+        }
+
+        private void SendMeasurementToServer(Measurement measurement)
+        {
+            // Implementiraj logiku za slanje merenja na server
+            Console.WriteLine($"Device {UniqueID}: Measurement sent to server. Value: {measurement.Value}, Timestamp: {measurement.Timestamp}");
+            foreach (Measurement m in Measurements)
+            {
+                
+            }
+
+           
         }
 
 
