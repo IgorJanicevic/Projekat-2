6394c36b120e3f8172db945da5c17428b7368806
IgorJanicevic <milkomilkic@yahoo.com>
2024-01-09 23:55:05 +0100

    Testovi device

--- ProjekatProxy/ProjectTest/Program.cs
+++ ProjekatProxy/ProjectTest/Program.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Moq;
+using ProjectTest.Testovi;
 using ProjekatProxy;
 
 namespace ProjectTest
@@ -22,15 +24,9 @@ namespace ProjectTest
             server.AcceptProxy();
             Console.WriteLine("----------------------------------");
 
-            // Provera starosti lokalne kopije
-            int intervalZaProveruLokalneKopije = int.Parse(ConfigurationManager.AppSettings["IntervalZaProveruLokalneKopije"]);
-            System.Threading.Timer timer = new System.Threading.Timer(LocalCopy, proxy, 0, intervalZaProveruLokalneKopije);
-
             //Lista uredjaja
             List<Device> devices = new List<Device>();
 
-            //string za izbor u hendleru
-            string temp1;
 
             //Kreiranje merenja svaki minut, za svaki uredjaj
             CreateMeasure cm = new CreateMeasure();
@@ -39,30 +35,14 @@ namespace ProjectTest
             SendMeasureToServerOn5Minutes sm = new SendMeasureToServerOn5Minutes();
 
 
+            //TESTOVI
 
-            do
-            {
+            DeviceTests deviceTests = new DeviceTests();
+            deviceTests.AddDeviceToList_ShouldAddDeviceToList(devices, server, cm, sm);
 
-                Console.WriteLine("\nIzaberi opciju: ");
-                Console.WriteLine("1 - Kreiraj novi device");
-                Console.WriteLine("2 - Klijentski meni");
-                Console.WriteLine("X - Izlaz\n");
-
-                temp1 = Console.ReadLine();
-
-                switch (temp1)
-                {
-                    case "1":
-                        AddDeviceToList(devices, server, cm, sm);
-                        break;
-                    case "2":
-                        ClientMenu(proxy, server);
-                        break;
-
-                }
-
-
-            } while (!temp1.ToUpper().Equals("X"));
+            ClientTests clientTests= new ClientTests(proxy,server);
+            //clientTests.TestZaKreiranjeKlijenta();
+            
 
 
 
@@ -78,62 +58,6 @@ namespace ProjectTest
 
 
         }
-
-
-
-        private static void ClientMenu(Proxy pp, Server server)
-        {
-            ch.Handler(pp, server);
-        }
-
-        //Dodavanje uredjaja u listu kao i kreiranje njegovih merenja i slanje na serveru
-        private static bool AddDeviceToList(List<Device> d, Server server, CreateMeasure cm, SendMeasureToServerOn5Minutes sm)
-        {
-            try
-            {
-                Console.Write("Unesi ID: ");
-                int devID = int.Parse(Console.ReadLine());
-                Device tempDev = new Device(devID);
-
-                foreach (Device dev in d)
-                {
-                    if (dev.UniqueID == devID)
-                    {
-                        Console.WriteLine("Vec postoji uredjaj sa tom ID oznakom");
-                        return false;
-                    }
-                }
-
-                d.Add(tempDev);
-                Console.WriteLine("Uspesno ste kreirali uredjaj");
-
-                //Kreiranje merenja svaki minut, za svaki uredjaj
-                cm.Create(tempDev);
-                Thread.Sleep(200); // Da slucajno ne dodje do preplitanja kreiranja merenja i slanja 
-
-                //Slanje svih merenja sa svih uredjaja koji se ne nalaze na serveru
-                sm.SendMeasure(server, tempDev);
-
-            }
-            catch (FormatException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            return true;
-
-        }
-
-        //Metoda za proveru starosti lokalne kopije
-        private static void LocalCopy(object state)
-        {
-
-            Proxy proxy = (Proxy)state;
-            proxy.CheckLocalCopyAge();
-        }
+       
     }
 }
---
+++ ProjekatProxy/ProjectTest/Testovi/DeviceTests.cs
@@ -0,0 +1,96 @@
+﻿using Moq;
+using NUnit.Framework;
+using ProjekatProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProjectTest.Testovi
+{
+    public class DeviceTests
+    {
+        //Test za unos ID-jeva uredjaja
+        private static bool AddDeviceToList(List<Device> d, Server server, CreateMeasure cm, SendMeasureToServerOn5Minutes sm,
+            IConsoleReader consoleReader)
+        {
+            try
+            {
+                Console.Write("Unesi ID: ");
+                int devID = int.Parse(consoleReader.ReadLine());
+                Device tempDev = new Device(devID);
+
+                foreach (Device dev in d)
+                {
+                    if (dev.UniqueID == devID)
+                    {
+                        Console.WriteLine("Vec postoji uredjaj sa tom ID oznakom");
+                        return false;
+                    }
+                }
+
+                d.Add(tempDev);
+                Console.WriteLine("Uspesno ste kreirali uredjaj");
+
+                //Kreiranje merenja svaki minut, za svaki uredjaj
+                cm.Create(tempDev);
+                Thread.Sleep(200); // Da slucajno ne dodje do preplitanja kreiranja merenja i slanja 
+
+                //Slanje svih merenja sa svih uredjaja koji se ne nalaze na serveru
+                sm.SendMeasure(server, tempDev);
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+
+            return true;
+
+        }
+      
+        public void AddDeviceToList_ShouldAddDeviceToList(List<Device> d,Server server,CreateMeasure cm,SendMeasureToServerOn5Minutes sm)
+        {
+            // Arrange
+            var mockConsoleReader = new Mock<IConsoleReader>();
+            mockConsoleReader.Setup(cr => cr.ReadLine()).Returns("ff"); // Simulacija unosa sa konzole
+
+            var yourClassInstance = new DeviceTests(); // Promenite ovo prema vašem stvarnom scenariju
+
+            // ... Dodajte ostale mock-ove i inicijalizaciju objekata ako je potrebno ...
+
+            // Act
+            var result = DeviceTests.AddDeviceToList(d, server, cm, sm, mockConsoleReader.Object);
+
+            // Assert
+            if(result == true )
+            {
+                Console.WriteLine("Greska klijent ne sme da unese slova za ID uredjaja");
+            }
+
+            mockConsoleReader.Setup(cr => cr.ReadLine()).Returns("2"); // Simulacija unosa sa konzole
+            result = DeviceTests.AddDeviceToList(d, server, cm, sm, mockConsoleReader.Object);
+
+            mockConsoleReader.Setup(cr => cr.ReadLine()).Returns("2"); // Simulacija unosa sa konzole
+            result = DeviceTests.AddDeviceToList(d, server, cm, sm, mockConsoleReader.Object);
+
+            if(result == true )
+            {
+                Console.WriteLine("Klijent ne sme da unese dva ista ID-ja");
+            }
+
+            Console.WriteLine("Uspesno su prosli testovi za unos id uredjaj");
+
+
+        }
+
+    }
+}
